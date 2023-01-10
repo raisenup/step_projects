370c232063a92e724babf8cc26ac65a0ce4f4c7b
Илья <port.qwert1ng@gmail.com>
2023-01-10 20:11:38 +0200

    init

---
+++ cpp/10.01/main.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void ex_1()
+{
+	int year, days;
+	cout << "Input year -> " << endl;
+	cin >> year;
+	days = 366 - year % 2;
+	cout << days << endl;
+}
+
+void ex_2()
+{
+	int grn, kop;
+	cout << "Input grn -> " << endl;
+	cin >> grn;
+	cout << "Input kop -> " << endl;
+	cin >> kop;
+	if (kop >= 100)
+	{
+		grn += kop / 100;
+		kop %= 100;
+	}
+	cout << grn << " grn " << kop << " kop" << endl;
+}
+
+void ex_3()
+{
+	double l, w, h, V;
+	cout << "Calculation of parallelepiped volume.\nInput data:\n";
+	cout << "\tLength (cm) -> ";
+	cin >> l;
+	cout << "\tWidth (cm) ->  ";
+	cin >> w;
+	cout << "\tHeight (cm) -> ";
+	cin >> h;
+	V = l * w * h;
+	cout << "\tVolume: " << V << " cubic cm" << endl;
+}
+
+void ex_4()
+{
+	double scale, l, distance;
+	cout << "Calculation of distance between towns.\nInput data:\n";
+	cout << "\tMap scale (kilometers per centimeter) -> ";
+	cin >> scale;
+	cout << "\tDistance between dots (cm) -> ";
+	cin >> l;
+	distance = scale * l;
+	cout << "\tDistance between the towns is " << distance << " km." << endl;
+}
+
+void ex_5()
+{
+	double pi = 3.141592, v, r;
+	cout << "Input R of the ball -> ";
+	cin >> r;
+	v = pi * 4 / 3 * r * r * r;
+	cout << "Volume of the ball is " << v << " cubic cm." << endl;
+}
+
+int main()
+{
+	int ex_num = 0;
+	cout << "Input exercise number: " << endl;
+	cin >> ex_num;
+	switch (ex_num)
+	{
+	case 1:
+		ex_1();
+		break;
+	case 2:
+		ex_2();
+		break;
+	case 3:
+		ex_3();
+		break;
+	case 4:
+		ex_4();
+		break;
+	case 5:
+		ex_5();
+		break;
+	}
+
+	return 0;
+}
+
+
